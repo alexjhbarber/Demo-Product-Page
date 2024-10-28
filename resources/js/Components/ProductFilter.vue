513c55d6c90884b3a6da936d67997d9497173af6
AJHBarber <alex8mpn@gmail.com>
2024-10-28 12:17:56 +0000

    feat: ui for desktop complete and pagination added

--- resources/js/Components/ProductFilter.vue
+++ resources/js/Components/ProductFilter.vue
@@ -1,137 +1,134 @@
 <template>
     <div class="w-[256px] flex flex-col rounded-lg border-gray-200 p-0">
     
-    <div class="p-4 flex flex-col align-start gap-3 align-sketch">
-        <button
-            class="flex justify-between items-center w-full scratch text-start text-gray-900 hover:text-gray-500 leading-tight text-base font-semibold"
-            @click="toggleFilter(filters.price)"
-        >
-            Price
-            <svg v-if="!filters.price.open" class="block size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                <path d="m6 9 6 6 6-6"></path>
-            </svg>
-            <svg v-else class="block size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                <path d="m18 15-6-6-6 6"></path>
-            </svg>
-        </button>
-        <div v-if="filters.price.open" class="overflow-hidden transition-[height] duration-300 flex flex-row flex-start gap-4">
-            <div class="w-[100px] flex flex-col flex-start gap-2 text-gray-900 text-sm font-medium">
-                From
-               <input
-                type="number"
-                min="0"
-                v-model="selectedMinPrice"
-                @change="onPriceChange"
-                class="flex py-2 px-4 items-center rounded-lg border border-gray-300 bg-grey-50 text-gray-500 text-sm font-normal"
-            /> 
-            </div>
-            <div class="w-[100px] flex flex-col flex-start gap-2 text-gray-900 text-sm font-medium">
-                To
-               <input
-                type="number"
-                min="0"
-                v-model="selectedMaxPrice"
-                @change="onPriceChange"
-                class="flex py-2 px-4 items-center rounded-lg border border-gray-300 bg-grey-50 text-gray-500 text-sm font-normal"
-            /> 
-            </div>
-            
-            
-        </div>
-    </div>
-    
-    <div>
-        <button
-            class="flex justify-between items-center w-full py-3 font-semibold text-start text-gray-800 hover:text-gray-500 rounded-lg"
-            @click="toggleFilter(filters.brand)"
-        >
-            Brand
-            <svg v-if="!filters.brand.open" class="block size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                <path d="m6 9 6 6 6-6"></path>
-            </svg>
-            <svg v-else class="block size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                <path d="m18 15-6-6-6 6"></path>
-            </svg>
-        </button>
-        <div v-if="filters.brand.open" class="overflow-hidden transition-[height] duration-300">
-            <div v-if="brands.length > 0" v-for="brand in brands" :key="brand">
-            <input
-                type="checkbox"
-                :value="brand"
-                v-model="selectedBrands"
-                @change="onBrandChange"
-            />
-            <label>{{ brand }}</label>
-        </div>
-        </div>
-    </div>
-
-    <div>
-        <button
-            class="flex justify-between items-center w-full py-3 font-semibold text-start text-gray-800 hover:text-gray-500 rounded-lg"
-            @click="toggleFilter(filters.categories)"
-        >
-            Categories
-            <svg v-if="!filters.categories.open" class="block size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                <path d="m6 9 6 6 6-6"></path>
-            </svg>
-            <svg v-else class="block size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                <path d="m18 15-6-6-6 6"></path>
-            </svg>
-        </button>
-        <div v-if="filters.categories.open" class="overflow-hidden transition-[height] duration-300">
-            <div v-if="categories.length > 0" v-for="category in categories" :key="category">
+        <div class="p-4 flex flex-col align-start gap-3 align-sketch">
+            <button
+                class="flex justify-between items-center w-full scratch text-start text-gray-900 hover:text-gray-500 leading-tight text-base font-semibold"
+                @click="toggleFilter(filters.price)"
+            >
+                Price
+                <svg v-if="!filters.price.open" class="block size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                    <path d="m6 9 6 6 6-6"></path>
+                </svg>
+                <svg v-else class="block size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                    <path d="m18 15-6-6-6 6"></path>
+                </svg>
+            </button>
+            <div v-if="filters.price.open" class="overflow-hidden transition-[height] duration-300 flex flex-row flex-start gap-4">
+                <div class="w-[100px] flex flex-col flex-start gap-2 text-gray-900 text-sm font-medium">
+                    From
                 <input
-                    type="checkbox"
-                    :value="category"
-                    v-model="selectedCategories"
-                    @change="onCategoryChange"
-                />
-                <label>{{ category }}</label>
-            </div> 
+                    type="number"
+                    min="0"
+                    v-model="selectedMinPrice"
+                    @change="onPriceChange"
+                    class="flex py-2 px-4 items-center rounded-lg border border-gray-300 bg-grey-50 text-gray-500 text-sm font-normal"
+                /> 
+                </div>
+                <div class="w-[100px] flex flex-col flex-start gap-2 text-gray-900 text-sm font-medium">
+                    To
+                <input
+                    type="number"
+                    min="0"
+                    v-model="selectedMaxPrice"
+                    @change="onPriceChange"
+                    class="flex py-2 px-4 items-center rounded-lg border border-gray-300 bg-grey-50 text-gray-500 text-sm font-normal"
+                /> 
+                </div>
+                
+                
+            </div>
         </div>
-    </div>
-
-    <div>
-        <button
-            class="flex justify-between items-center w-full py-3 font-semibold text-start text-gray-800 hover:text-gray-500 rounded-lg"
-            @click="toggleFilter(filters.condition)"
-        >
-            Condition
-            <svg v-if="!filters.condition.open" class="block size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                <path d="m6 9 6 6 6-6"></path>
-            </svg>
-            <svg v-else class="block size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                <path d="m18 15-6-6-6 6"></path>
-            </svg>
-        </button>
-        <div v-if="filters.condition.open" class="overflow-hidden transition-[height] duration-300">
-            
-        </div>
-    </div>
-
-    <div>
-        <button
-            class="flex justify-between items-center w-full py-3 font-semibold text-start text-gray-800 hover:text-gray-500 rounded-lg"
-            @click="toggleFilter(filters.rating)"
-        >
-            Rating
-            <svg v-if="!filters.rating.open" class="block size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                <path d="m6 9 6 6 6-6"></path>
-            </svg>
-            <svg v-else class="block size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                <path d="m18 15-6-6-6 6"></path>
-            </svg>
-        </button>
-        <div v-if="filters.rating.open" class="overflow-hidden transition-[height] duration-300">
-            
-        </div>
-    </div>
-
-
-      
         
-        
+        <div class="p-4 pt-0 flex flex-col align-start gap-3 align-sketch ">
+            <button
+                class="flex justify-between items-center w-full font-semibold text-start text-gray-800 hover:text-gray-500 rounded-lg"
+                @click="toggleFilter(filters.brand)"
+            >
+                Brand
+                <svg v-if="!filters.brand.open" class="block size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                    <path d="m6 9 6 6 6-6"></path>
+                </svg>
+                <svg v-else class="block size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                    <path d="m18 15-6-6-6 6"></path>
+                </svg>
+            </button>
+            <div v-if="filters.brand.open" class="overflow-hidden transition-[height] duration-300">
+                <div v-if="brands.length > 0" v-for="brand in brands" :key="brand" class="flex p-0 items-center gap-2 mb-3">
+                    <input
+                        type="checkbox"
+                        :value="brand"
+                        v-model="selectedBrands"
+                        @change="onBrandChange"
+                        class="flex w-4 h-4 rounded border-gray-300 bg-grey-50"
+                    />
+                    <label class="text-gray-900 text-sm font-medium">{{ brand }}</label>
+                </div>
+            </div>
+        </div>
+
+        <div class="p-4 pt-0 flex flex-col align-start gap-3 align-sketch">
+            <button
+                class="flex justify-between items-center w-full font-semibold text-start text-gray-800 hover:text-gray-500 rounded-lg"
+                @click="toggleFilter(filters.categories)"
+            >
+                Categories
+                <svg v-if="!filters.categories.open" class="block size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                    <path d="m6 9 6 6 6-6"></path>
+                </svg>
+                <svg v-else class="block size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                    <path d="m18 15-6-6-6 6"></path>
+                </svg>
+            </button>
+            <div v-if="filters.categories.open" class="overflow-hidden transition-[height] duration-300">
+                <div v-if="categories.length > 0" v-for="category in categories" :key="category" class="flex p-0 items-center gap-2 mb-3">
+                    <input
+                        type="checkbox"
+                        :value="category"
+                        v-model="selectedCategories"
+                        @change="onCategoryChange"
+                        class="flex w-4 h-4 rounded border-gray-300 bg-grey-50"
+                    />
+                    <label class="text-gray-900 text-sm font-medium">{{ category }}</label>
+                </div> 
+            </div>
+        </div>
+
+        <div class="p-4 pt-0 flex flex-col align-start gap-3 align-sketch">
+            <button
+                class="flex justify-between items-center w-full font-semibold text-start text-gray-800 hover:text-gray-500 rounded-lg"
+                @click="toggleFilter(filters.condition)"
+            >
+                Condition
+                <svg v-if="!filters.condition.open" class="block size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                    <path d="m6 9 6 6 6-6"></path>
+                </svg>
+                <svg v-else class="block size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                    <path d="m18 15-6-6-6 6"></path>
+                </svg>
+            </button>
+            <div v-if="filters.condition.open" class="overflow-hidden transition-[height] duration-300">
+                
+            </div>
+        </div>
+
+        <div class="p-4 pt-0 flex flex-col align-start gap-3 align-sketch">
+            <button
+                class="flex justify-between items-center w-full font-semibold text-start text-gray-800 hover:text-gray-500 rounded-lg"
+                @click="toggleFilter(filters.rating)"
+            >
+                Rating
+                <svg v-if="!filters.rating.open" class="block size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                    <path d="m6 9 6 6 6-6"></path>
+                </svg>
+                <svg v-else class="block size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                    <path d="m18 15-6-6-6 6"></path>
+                </svg>
+            </button>
+            <div v-if="filters.rating.open" class="overflow-hidden transition-[height] duration-300">
+                
+            </div>
+        </div>     
     </div>
 </template>
 
@@ -158,8 +155,8 @@
                 },
                 categories: [],
                 brands: [],
-                selectedCategories: [],
-                selectedBrands: [],
+                selectedCategories: new URLSearchParams(window.location.search).get('category')?.split(',') ?? [],
+                selectedBrands: new URLSearchParams(window.location.search).get('brand')?.split(',') ?? [],
                 selectedMinPrice: this.minPrice,
                 selectedMaxPrice: this.maxPrice,
             };
@@ -173,6 +170,7 @@
                 } catch (error) {
                     console.error(error);
                 }
+                
             },
             onCategoryChange() {
                 this.$emit('update-filters', 'category', this.selectedCategories);
--- resources/js/Components/ProductList.vue
+++ resources/js/Components/ProductList.vue
@@ -1,13 +1,33 @@
 <template>
-        <div class="flex w-[var(--xl, 1280px)] items-start gap-4">
+    <div class="flex w-[var(--xl, 1280px)] items-start gap-4">
         <product-filter
             @update-filters="updateFilterParams"
             @update-filters-price="updatePriceRange"
+            
             multiple
         />
-        
-        <div class="w-full flex flex-wrap gap-2.5" v-if="products.length">
-            <product-item v-for="product in products" :key="product.id" :product="product" />
+        <div class="w-[1008px] flex flex-wrap gap-2.5">
+            <p class="mb-4 text-2xl font-semibold text-gray-900">Featured Deals</p>
+            <div class="w-full flex flex-wrap gap-2.5" v-if="products.length">
+                <product-item v-for="product in products" :key="product.id" :product="product" />
+            </div>
+
+            <div class="w-full mt-12 flex justify-center mb-2">
+                <button
+                    :class="['w-[82px] text-sm font-medium text-gray-500 px-3 py-1 border rounded rounded-r-none  border-gray-300 items-center flex-col flex', { 'bg-gray-300': productMeta.current_page === 1 }]"
+                    :disabled="productMeta.current_page === 1"
+                    @click="newPage(productMeta.current_page - 1)"
+                >
+                    Previous
+                </button>
+                <button
+                    :class="['w-[82px] text-sm font-medium text-gray-500 px-3 py-1 border rounded rounded-l-none border-gray-300 items-center flex-col flex', { 'bg-gray-300': productMeta.current_page === productMeta.last_page }]"
+                    :disabled="productMeta.current_page === productMeta.last_page"
+                    @click="newPage(productMeta.current_page + 1)"
+                >
+                    Next
+                </button>
+            </div>
         </div>
     </div>
     
@@ -26,6 +46,7 @@ export default {
     },
     data() {
         return {
+            productMeta: [],
             products: [],
             selectedCategories: [],
             selectedBrands: [],
@@ -41,9 +62,12 @@ export default {
                         brand: params.get('brand') || '',
                         min_price: params.get('minPrice') || '',
                         max_price: params.get('maxPrice') || '',
+                        page: params.get('page') ,
                     },
                 });
+                
                 this.products = response.data.data;
+                this.productMeta = response.data;
             } catch (error) {
                 console.error(error);
             }
@@ -58,6 +82,12 @@ export default {
             const params = new URLSearchParams(window.location.search);
             params.set('minPrice', min);
             params.set('maxPrice', max);
+            history.replaceState({}, '', `${window.location.pathname}?${params}`);
+            this.fetchProducts();
+        },
+        newPage(page) {
+            const params = new URLSearchParams(window.location.search);
+            params.set('page', page);
             history.replaceState({}, '', `${window.location.pathname}?${params}`);
             this.fetchProducts();
         },

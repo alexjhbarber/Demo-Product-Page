d0e4df5812bbcb14071d1ac0d891451ea30543ab
AJHBarber <alex8mpn@gmail.com>
2024-11-04 15:08:32 +0000

    fix: update initial install process

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,11 @@ use Inertia\Inertia;
 */
 
 Route::get('/', function () {
-    return Inertia::render('Home', []);
+    return Inertia::render('Home', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
 });
-
-
-
 require __DIR__.'/auth.php';
